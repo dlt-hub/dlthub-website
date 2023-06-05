20625631e8ee77d56f05f05b10a3503409029ed1
Dave <shrps@posteo.net>
2023-06-05 14:15:22 +0200

    update lockfiles add env selection

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ require("dotenv").config({
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  environment: process.env.CONTENTFUL_ENVIRONMENT,
 };
 
 if (process.env.CONTENTFUL_HOST) {
