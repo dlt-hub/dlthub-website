05d7a0cd061d5f5268f034cfdad14515aac9e841
Elise Boyd <elise@eliseboyd.com>
2024-05-24 16:58:38 +0200

    add contact sales

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,6 +46,7 @@ export default function Layout(props) {
             <a href="/success-stories">success stories</a>
             <a href="/about">about</a>
             <a href="/docs/intro">docs</a>
+            <a href="https://info.dlthub.com/contact-sales">contact sales</a>
           </div>
           <div className={styles.images}>
             <a
